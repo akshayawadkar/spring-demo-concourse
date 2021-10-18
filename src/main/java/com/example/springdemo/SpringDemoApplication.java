a8e1da94a8c0ce665a8217076501592bf776a44d
akshayanilwadkar <mailaw3515@gmail.com>
2021-10-17 19:20:49 -0700

    init commit

---
+++ src/main/java/com/example/springdemo/SpringDemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.springdemo;
+
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class SpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoApplication.class, args);
+
+        log.info("Hi from Spring Boot Local");
+    }
+
+}
